d9c096e323e6cb2c85b9127a965a73c0e3d47d0e
extremecode <akash.gupta2@microfocus.com>
2021-11-03 20:08:24 +0530

    Brininging postgres database in test enviroment

--- de-auth-server-image/src/dist/docker/boot.sh
+++ de-auth-server-image/src/dist/docker/boot.sh
@@ -12,6 +12,44 @@ if [[ -z "${DEPLOYMENT_INSTALL_DIR}" ]]; then
     DEPLOYMENT_INSTALL_DIR="/opt/DE"
 fi
 
+if [[ -z "${DEPLOYMENT_DB_SVC_NAME}" ]]; then
+    echo "`date +\"%F %T\"` [INFO] DE service variable DEPLOYMENT_DB_SVC_NAME is not set. Default value 'localhost' will be used."
+    DEPLOYMENT_DB_SVC_NAME="localhost"
+fi
+
+if [[ -z "${DEPLOYMENT_DB_SVC_PORT}" ]]; then
+    echo "`date +\"%F %T\"` [INFO] DE service variable DEPLOYMENT_DB_SVC_PORT is not set. Default value '5432' will be used."
+    DEPLOYMENT_DB_SVC_PORT="5432"
+fi
+
+if [[ -z "${DEPLOYMENT_DB_SVC_DATABASE_NAME}" ]]; then
+    echo "`date +\"%F %T\"` [INFO] DE service variable DEPLOYMENT_DB_SVC_DATABASE_NAME is not set. Default value 'postgres' will be used."
+    DEPLOYMENT_DB_SVC_DATABASE_NAME="postgres"
+fi
+
+if [[ -z "${DEPLOYMENT_DB_SVC_USER_NAME}" ]]; then
+    echo "`date +\"%F %T\"` [INFO] DE service variable DEPLOYMENT_DB_SVC_USER_NAME is not set. Default value 'postgres' will be used."
+    DEPLOYMENT_DB_SVC_USER_NAME="postgres"
+fi
+
+if [[ -z "${DEPLOYMENT_DB_SVC_PASSWORD}" ]]; then
+    echo "`date +\"%F %T\"` [INFO] DE service variable DEPLOYMENT_DB_SVC_PASSWORD is not set. Default value 'postgres' will be used."
+    DEPLOYMENT_DB_SVC_PASSWORD="postgres"
+fi
+
+DEPLOYMENT_DB_JDBC_URL="jdbc:postgresql://$DEPLOYMENT_DB_SVC_NAME:$DEPLOYMENT_DB_SVC_PORT/$DEPLOYMENT_DB_SVC_DATABASE_NAME"
+
+
+echo "`date +\"%F %T\"` [INFO] Will use JDBC URL:$DEPLOYMENT_DB_JDBC_URL"
+
+echo "
+
+spring.datasource.url= $DEPLOYMENT_DB_JDBC_URL
+spring.datasource.username= $DEPLOYMENT_DB_SVC_USER_NAME
+spring.datasource.password= $DEPLOYMENT_DB_SVC_PASSWORD
+
+" >> $DEPLOYMENT_INSTALL_DIR/application.properties
+
 echo "`date +\"%F %T\"` [INFO] Setting server SSL properties."
 echo "security.require-ssl=true
 server.ssl.protocol=TLS
