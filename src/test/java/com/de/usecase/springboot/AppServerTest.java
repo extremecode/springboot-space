5019df72f8edbf87ecc619929d32f94555927fee
guptakas <akash.gupta2@microfocus.com>
2021-11-03 14:06:07 +0530

    Containerized spring boot

---
+++ src/test/java/com/de/usecase/springboot/AppServerTest.java
@@ -0,0 +1,29 @@
+package com.de.usecase.springboot;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import static org.junit.Assert.*;
+
+@Category(IntegrationTest.class)
+public class AppServerTest {
+
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void home() {
+
+    }
+
+}
