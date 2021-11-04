8a2cce218329640660a47a42f2c149d1a9ce2ffe
extemecode <akash.gupta2@microfocus.com>
2021-11-04 14:24:13 +0530

    Create seprate sub dir structure for sepration

--- de-auth-server-image/src/dist/docker/startup/boot.d/320-create-keystore.sh
+++ de-auth-server-image/src/dist/docker/startup/boot.d/320-create-keystore.sh
@@ -14,6 +14,6 @@ fi
 
 
 $JAVA_HOME/bin/keytool -genkey -alias de -keyalg rsa -sigalg SHA256withRSA -keysize 2048 -dname "CN=$DEPLOYMENT_HOSTNAME,OU=DE,O=DE,L=Bangalore,ST=KA,C=India" -keystore $DEPLOYMENT_CONF_DIR/de.keystore -keypass dataelicit@01 -storepass dataelicit@01 -storetype PKCS12
-$JAVA_HOME/bin/keytool -export -alias de -file $DEPLOYMENT_CONF_DIR/de.cer -keystore $DEPLOYMENT_INSTALL_DIR/de.keystore -storetype PKCS12 -storepass dataelicit@01
-$JAVA_HOME/bin/keytool -import -alias de -file $DEPLOYMENT_CONF_DIR/de.cer -noprompt -keystore $DEPLOYMENT_INSTALL_DIR/de.truststore -storetype PKCS12 -storepass dataelicit@01
+$JAVA_HOME/bin/keytool -export -alias de -file $DEPLOYMENT_CONF_DIR/de.cer -keystore $DEPLOYMENT_CONF_DIR/de.keystore -storetype PKCS12 -storepass dataelicit@01
+$JAVA_HOME/bin/keytool -import -alias de -file $DEPLOYMENT_CONF_DIR/de.cer -noprompt -keystore $DEPLOYMENT_CONF_DIR/de.truststore -storetype PKCS12 -storepass dataelicit@01
 
