a74cf4a386c27937b269408f9966034abba399da
extemecode <akash.gupta2@microfocus.com>
2021-11-04 18:36:38 +0530

    external properties

--- de-auth-server-image/src/dist/docker/startup/boot.sh
+++ de-auth-server-image/src/dist/docker/startup/boot.sh
@@ -3,26 +3,28 @@
 set -e
 umask 0027
 
-echo "`date +\"%F %T\"` [INFO] Running DE Auth server boot scripts..."
+./helper.sh
+
+loginfo "Running DE Auth server boot scripts..."
 for script in $(ls -1 $DE_BOOT/*.sh)
 do
 	. $script;
 done
-echo "`date +\"%F %T\"` [INFO] Completed execution of all DE Auth server boot scripts..."
+loginfo "Completed execution of all DE Auth server boot scripts..."
 
 if [[ -z "${DEPLOYMENT_CONF_DIR}" ]]; then
-    echo "`date +\"%F %T\"` [INFO] DE service variable DEPLOYMENT_CONF_DIR is not set. Default value '/opt/DE/conf' will be used."
+    loginfo "DE service variable DEPLOYMENT_CONF_DIR is not set. Default value '/opt/DE/conf' will be used."
     DEPLOYMENT_CONF_DIR="/opt/DE/conf"
 fi
 
 if [[ -z "${DEPLOYMENT_LIB_DIR}" ]]; then
-    echo "`date +\"%F %T\"` [INFO] DE service variable DEPLOYMENT_LIB_DIR is not set. Default value '/opt/DE/lib' will be used."
+    loginfo "DE service variable DEPLOYMENT_LIB_DIR is not set. Default value '/opt/DE/lib' will be used."
     DEPLOYMENT_LIB_DIR="/opt/DE/lib"
 fi
 
 
 
-echo "`date +\"%F %T\"` [INFO] Starting DE AUTH server..."
+loginfo "Starting DE AUTH server..."
 exec ${JAVA_HOME}/jre/bin/java -Dspring.config.location=file:$DEPLOYMENT_CONF_DIR/application.properties  -Djavax.net.ssl.trustStorePassword=dataelicit@01 -Djavax.net.ssl.trustStore=$DEPLOYMENT_CONF_DIR/de.truststore  -jar $DEPLOYMENT_LIB_DIR/app.jar
 
 
